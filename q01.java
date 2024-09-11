8e801eded34e25b6904ed31a503c2e32a5f891ab
IvK1fs <166817912+IvK1fs@users.noreply.github.com>
2024-09-10 22:22:07 -0300

    Create q01.java

---
+++ q01.java
@@ -0,0 +1,26 @@
+mport java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+
+
+    System.out.println("Insira o primeiro número: ");
+
+    Scanner entrada = new Scanner(System.in);
+
+    int n1 = entrada.nextInt();
+
+
+
+    System.out.println("Insira o segundo número: ");
+
+
+
+    int n2 = entrada.nextInt();
+
+    int calc = n1 + n2;
+    System.out.println("A soma dos números é igual a "+ calc);
+
+    entrada.close();
+}
+}
